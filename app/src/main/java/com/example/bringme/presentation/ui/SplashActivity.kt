946de07ca9c12f73e3bce55bda0b43667cc088b2
DESKTOP-L4EUC03\ArtemSh <artem_shendikov@mail.ru>
2021-03-19 10:10:35 +0300

    add splash activity

---
+++ app/src/main/java/com/example/bringme/presentation/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bringme.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bringme.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
